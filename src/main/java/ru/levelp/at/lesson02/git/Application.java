4688bed1a4ad39df6ca65c84c26c8de86fb247e1
d.khodakovsky <khda91@gmail.com>
2022-05-26 20:49:10 +0300

    Revert "added add for double values"
    This reverts commit 98665d6e6d42c50002fcacbefcb4f8cbaede7d43.

--- src/main/java/ru/levelp/at/lesson02/git/Application.java
+++ src/main/java/ru/levelp/at/lesson02/git/Application.java
@@ -6,9 +6,8 @@ public class Application {
         Calculator calculator = new Calculator();
         System.out.println("Сложение");
         System.out.println("2 + 2 = " + calculator.add(2, 2));
-        System.out.println("2.03 + 2.03 = " + calculator.add(2.03, 2.03));
         System.out.println();
-
+        
         System.out.println("Вычитание");
         System.out.println("4 - 2 = " + calculator.subtract(4, 2));
         System.out.println();
--- src/main/java/ru/levelp/at/lesson02/git/Calculator.java
+++ src/main/java/ru/levelp/at/lesson02/git/Calculator.java
@@ -9,8 +9,4 @@ public class Calculator {
     public int subtract(int a, int b) {
         return a - b;
     }
-
-    public double add(double a, double b) {
-        return a + b;
-    }
 }
