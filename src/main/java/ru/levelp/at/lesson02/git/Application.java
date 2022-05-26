39b48e430e3cbb6d7562423cc0532a860af9a13b
d.khodakovsky <khda91@gmail.com>
2022-05-26 21:19:59 +0300

    added divide method for ints

--- src/main/java/ru/levelp/at/lesson02/git/Application.java
+++ src/main/java/ru/levelp/at/lesson02/git/Application.java
@@ -18,5 +18,9 @@ public class Application {
         System.out.println("4 * 2 = " + calculator.multiply(4, 2));
         System.out.println("4.025 * 2.336 = " + calculator.multiply(4.025, 2.336));
         System.out.println();
+
+        System.out.println("Деление");
+        System.out.println("4 / 2 = " + calculator.divide(4, 2));
+        System.out.println();
     }
 }
--- src/main/java/ru/levelp/at/lesson02/git/Calculator.java
+++ src/main/java/ru/levelp/at/lesson02/git/Calculator.java
@@ -25,4 +25,8 @@ public class Calculator {
     public double multiply(double a, double b) {
         return a * b;
     }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
 }
